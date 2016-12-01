8c616f5ccf8b862cde287b0bae6198123610f5c9
RobotBo <harrison.skalski@gmail.com>
2016-12-01 16:49:12 -0600

    Started Work on neural net

--- Neural Net/NeuralNet.h
+++ Neural Net/NeuralNet.h
@@ -1,5 +1,29 @@
+struct Neuron{
+	//Number of inputs to the given Neuron
+	int numInputs;
+
+	//Array of weights from each input
+	vector<double> vecWeight;
+
+	//contstructor
+	Neuron(int nI);
+};
+
+struct NeuronLayer{
+
+	//number neurons in this layer
+
+	int numNeurons;
+
+	//layer of neurons
+
+	vector<Neuron> vecNeurons;
+
+	//constructor
+	NeuronLayer(int nNurons, int numInputsPerNeuron);
+};
+
 
 class NeuralNet{
 
-	
 };
