b91c68b13149f1fe8a6ec7c34a6971ed51bd33af
RobotBo <harrison.skalski@gmail.com>
2016-12-08 13:54:40 -0600

    Merged all Neural net folders into 'NeuralNet'

---
+++ NeuralNet/NeuralNet.h
@@ -0,0 +1,64 @@
+struct Neuron{
+	/**
+	 * Number of inputs to the given Neuron
+	 */
+	int numInputs;
+	//Array of weights from each input
+	vector<double> vecWeight;
+
+	//contstructor
+	Neuron(int nI);
+};
+
+struct NeuronLayer{
+
+	//number neurons in this layer
+
+	int numNeurons;
+
+	//layer of neurons
+
+	vector<Neuron> vecNeurons;
+
+	//constructor
+	NeuronLayer(int nNurons, int numInputsPerNeuron);
+};
+
+
+class NeuralNet{
+
+private:
+
+	int numInputs;
+
+	int numOutputs;
+
+	int numHiddenLayers;
+
+	int neuronsPerHiddenLyr;
+
+	//Storage for Layers of nerons, includes output
+	vector<NeuronLayer> vecLayers;
+
+public:
+
+	NeuralNet();
+
+	void CreateNet();
+
+	// Gets the weight from neural net
+	vector<double> GetWeights()const;
+
+	// returns total number of weights in the net
+	int GetNumberOfWeights()const;
+
+	// replaces weights w/ new ones;
+	int PutWeights(vector<double> &weights);
+
+	// calc outputs from set of inputs;
+	vector<double> Update(vector<double> &inputs);
+
+	// sigmoid response curve
+	inline double Sigmoid(double activation, double response);
+
+};
---
+++ NeuralNet/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['numinputs',['numInputs',['../struct_neuron.html#a09992eeceb974593f5bf557aae581d2d',1,'Neuron']]]
+];
