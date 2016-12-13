2c43c981e7b746c142ff86a12ab47ac0eaa65a80
jedmeyer <jedmeyer@stolaf.edu>
2016-12-13 23:17:11 +0000

    Stuff

--- NeuralNet/GenAlg.cpp
+++ NeuralNet/GenAlg.cpp
@@ -9,13 +9,11 @@
 #include "Params.h"
 using namespace std;
 
-
-using namespace std;
-
 genome::genome(){fitness = 0;}
 
 genome::genome(double f1){
-	fitness = f1;};
+	fitness = f1;
+};
 
 genome::genome(vector<double> w1, double f1){
 	chromoWeights = w1;
