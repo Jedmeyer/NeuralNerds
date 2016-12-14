6c8bd64b3b36b80baaf299987e6395f104807d22
RobotBo <harrison.skalski@gmail.com>
2016-12-14 15:58:34 -0600

    Terminator has been created

--- NeuralNet/controller.cpp
+++ NeuralNet/controller.cpp
@@ -12,34 +12,44 @@
 using namespace std;
 
 int main(){
+	Params p;
 	vector<double> inputs;
 	vector<double> outputs;
 	int popSize = Params::pop;
 	cout<<"HELP ME"<<endl;
-
+	cout<<popSize<<endl;
 	srand( static_cast<uint>( time( NULL ) ) );
 
 	NeuralNet net;//declares the neural network object
 	GenAlg gen(net);//Declares the GenAlg object and fills the population
+
 	vector<double> GWeights;
+
 	while(true){//This will be the start of the main loop for the genalg.
+		
 		for(int i = 0; i< popSize; i++){//This is the loop for each genome in the generation
+
 			GWeights = gen.population[i].chromoWeights;//Gets the Weigths for this genome
 			g2048 g;
 			g.addTile();//This initializes the 2048 board
+
 		  while( true )//This is the 2048 game loop
 		  {
 		    if( g.moved ) g.addTile();
 		    g.drawBoard();
 		    if( g.done ) break;
 		    //g.waitKey();
+
 				inputs = g.toInput();
 				outputs = net.Update(inputs);
-				
-				if(totInput > 0){totInput = 1;}
-				else if(totInput < -1){totInput = 0;}
-				else{cout<<"Those Number are UNREAL Man"<<endl;}
 
+				/// Changes outputs into two bit binary format
+				for(int i = 0; i < outputs.size(); ++i){
+
+					if(outputs[i] > 0){outputs[i] = 1;}
+					else if(outputs[i] < -1){outputs[i] = 0;}
+					else{cout<<"Those Number are UNREAL Man"<<endl;}
+				}
 				g.netMove(outputs);
 		  }
 		  string s = "Game Over!";
